f9879d9342a7542534fdaa107ceb67e4119f68aa
Sunil Karki <karkisunil1200@gmail.com>
2020-03-16 16:08:19 -0500

    Getting it from id

--- accounts/account-router.js
+++ accounts/account-router.js
@@ -15,4 +15,21 @@ router.get('/', (req, res) => {
     });
 });
 
+router.get('/:id', (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  db('accounts')
+    .where({id})
+    .then(account => {
+      if (account) {
+        res.status(200).json({data: account});
+      } else {
+        res.status(404).json({message: 'Account not found'});
+      }
+    })
+    .catch(error => {
+      res.status(500).json({message: 'Something went wrong', error: error.message});
+    });
+});
+
 module.exports = router;
